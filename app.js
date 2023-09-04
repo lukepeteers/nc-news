3e313e89e3cc45d88a68ad8ccb885964eec2adf9
lukepeteers <lukepeters42@gmail.com>
2023-09-04 15:50:59 +0100

    add files for hosting

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const {getTopics, getServerDocs, getArticle, getAllArticles, getCommentsByArticleId, addComment, getArticleToPatch} = require('./controllers/controller')
 
-
+app.use(cors())
 app.use(express.json())
 app.get('/api/topics', getTopics)
 app.get('/api', getServerDocs)
